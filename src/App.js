423b74c21a80eb09ed7c6a12fa1a12d2baa63341
khoi148 <khoiminhphan@yahoo.com>
2020-05-11 01:53:04 +0700

    basic featuers up and running, milestones1-4 for wed lecture

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useRef } from "react";
+import "./App.css";
+import socketIOClient from "socket.io-client";
+const socket = socketIOClient("http://localhost:5000");
 
 function App() {
+  const [chat, setChat] = useState("");
+  const [name, setName] = useState("");
+  const [chatLog, setChatLog] = useState([]);
+  const chatLogRef = useRef(chatLog);
+
+  function submitChat(e) {
+    e.preventDefault();
+    const chatObj = {
+      text: e.target.chat.value,
+      name: name,
+    };
+    console.log("chatObj on submit: ", chatObj);
+    socket.emit("chat", chatObj);
+  }
+
+  useEffect(() => {
+    const user = prompt("please enter your name");
+    setName(user);
+    chatConnection();
+  }, []);
+
+  function chatConnection() {
+    socket.on("chatlog", (msg) => {
+      console.log("React chatlog", msg);
+      //chatLogRef.current is actually the current value of chatLog
+      chatLogRef.current.push(msg);
+      setChatLog([...chatLogRef.current]);
+    });
+  }
+
+  function renderChatLog() {
+    return chatLog.map((msg) => {
+      return <li>{msg}</li>;
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
+      <div className="chatbox">
+        <form onSubmit={(e) => submitChat(e)}>
+          <input name="chat" type="text" />
+          <button type="submit">Chat</button>
+        </form>
+      </div>
+      <div className="text-section">
+        <ul>
+          {chatLog &&
+            chatLog.map((msg) => {
+              return <li>{msg}</li>;
+            })}
+        </ul>
+      </div>
     </div>
   );
 }
